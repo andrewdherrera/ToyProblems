960868a9a97bd2a4dfd4d197146bf7c981f452c9
Andrew Herrera <thinblueline2830@gmail.com>
2019-06-27 10:08:37 -0500

    completed mergesort

---
+++ 27TP.js
@@ -0,0 +1,58 @@
+function mergeSort(arr){
+    if(arr.length < 2) {return arr};
+
+    let middle = Math.floor(arr.length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right){
+    let result = [];
+    
+    while(left.length && right.length){
+        if (left[0] <= right[0]){
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    while(left.length){
+        result.push(left.shift());
+    }
+    while(right.length){
+        result.push(right.shift());
+    }
+    return result;
+}
+const test1 = mergeSort([8,7,3,6,9,2,4,5,1]);
+console.log(`Should sort a short array of integers: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test1)}`);
+
+const test2 = mergeSort([8,7,3,3,9,2,4,5,1]);
+console.log(`Should handle duplicates: expected [1,2,3,3,4,5,7,8,9] and got ${JSON.stringify(test2)}`);
+
+const test3 = mergeSort([9,8,7,6,5,4,3,2,1]);
+console.log(`Should handle reversely sorted array: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test3)}`);
+
+const test4 = mergeSort([]);
+console.log(`Should handle empty array: expected [] and got ${JSON.stringify(test4)}`);
+
+const test5 = mergeSort([1]);
+console.log(`Should handle array with single element: expected [1] and got ${JSON.stringify(test5)}`);
+
+function createLargeArray() {
+  var input = [];
+  var sorted;
+  var n = 100000;
+  for (var i = 0; i < n; i++) {
+    var number = Math.floor(Math.random() * n);
+    input.push(number);
+  }
+
+  return input;
+}
+
+const arr = createLargeArray();
+const test7 = arr.slice().sort((a, b) => a - b);
+const test6 = mergeSort(arr);
+console.log(`Should handle large array: ${JSON.stringify(test6) === JSON.stringify(test7)}`);
